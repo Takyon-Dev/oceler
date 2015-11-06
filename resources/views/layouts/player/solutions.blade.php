966e131e99bc029452521c8125e034d4b4cb7334
Gabe Mansur <gabe_mansur@harvard.edu>
2015-11-06 16:25:05 -0500

    Added solution form to solutions table

--- resources/views/layouts/player/solutions.blade.php
+++ resources/views/layouts/player/solutions.blade.php
@@ -22,7 +22,23 @@
 						{{-- This span holds the most recent solution for this user in this solution category --}}
 						<span class="solution_container" id="sol_{{ $user->id }}_{{ $cat->id }}"></span>
 						{{-- This span holds the most recent solution's confidence level for this user in this solution category --}}
-						<span class="solution_container" id="conf_{{ $user->id }}_{{ $cat->id }}"></span>						
+						<span class="solution_container" id="conf_{{ $user->id }}_{{ $cat->id }}"></span>	
+						<form class="solution_form">
+							<label for="solve_input">{{ $cat->name }}:</label>
+							<input type="text" name="solve_input" class="solve_input_edit" value=""><br>
+							<label for="confidence">Confidence:</label>
+							<select name="confidence">
+								<option value="">----</option>
+								@for($i=10; $i <= 100; $i += 10)
+									<option value="{{$i}}">{{$i}}%</option>;
+								@endfor
+							</select><br>
+							<span class="err_msg"></span>
+							<input type="hidden" name="sol_cat" val="{{ $cat->id }}">
+							<input type="hidden" name="u_id" val="{{ $user->id }}">
+							<input class="btn btn-primary btn-sm pull-right" type="submit" name="solve_save" value="SAVE">
+							<input class="btn btn-primary btn-sm " type="submit" name="solve_cancel" value="CANCEL">
+						</form>	
 	            	</td>
 	            @endforeach	   
 
