02c8b6df4fb8a969a06826331c7d97ca904c367e
Gabe Mansur <gabe_mansur@harvard.edu>
2015-11-18 14:02:11 -0500

    Inital work on messages ajax code

--- public/js/messages.js
+++ public/js/messages.js
@@ -1,3 +1,20 @@
 $(document).ready(function() {
 
+
+	$(document).on('click', '#msg_send' , function(event) {
+
+		sendMessage();
+		event.preventDefault();
+	});
+
 });
+
+
+function sendMessage()
+{
+
+
+	form = $('#msg_form');
+	data = form.serialize();
+
+}
