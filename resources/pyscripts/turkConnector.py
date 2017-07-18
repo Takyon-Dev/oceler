1755ee9eecaf2d8e886d60d80a5b0f8956064f9a
Gabe Mansur <gabe_mansur@harvard.edu>
2017-07-18 15:11:35 -0400

    Added price object to boto grant_bonus

--- resources/pyscripts/turkConnector.py
+++ resources/pyscripts/turkConnector.py
@@ -44,7 +44,7 @@ else:
 if float(args.bonus) > 0:
         mturk.grant_bonus(worker_id = args.worker,
                           assignment_id = args.assignment,
-                          bonus_price = boto.mturk.price.Price( amount = args.bonus))
+                          bonus_price = (boto.mturk.price.Price( amount = args.bonus)))
         f.write('Worker: ' + args.worker + ' -- paying bonus ' + args.bonus + "\r\n")
 
 if args.trial_passed == 'true':
