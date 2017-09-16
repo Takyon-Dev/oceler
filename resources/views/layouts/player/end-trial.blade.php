284cb21b64965362a35889b15bff9bbb58876c57
Gabe Mansur <gabe_mansur@harvard.edu>
2017-09-16 15:01:25 -0400

    Changed end trial payout to just be total amt earned

--- resources/views/layouts/player/end-trial.blade.php
+++ resources/views/layouts/player/end-trial.blade.php
@@ -12,9 +12,7 @@
       <div class="text-center">
         <h3>This is the end of the experiment. Thank you for your participation!</h3>
         <h3>
-          You earned ${{ $total_earnings['bonus'] }} for your performance
-          in addition to your base payment of ${{ $total_earnings['base_pay'] }}
-          for a total of ${{ ($total_earnings['bonus'] + $total_earnings['base_pay']) }}
+          You earned a total of ${{ ($total_earnings['bonus'] + $total_earnings['base_pay']) }}
         </h3>
           @if($assignment_id)
             @include('layouts.includes.mturk-external-submit-form')
