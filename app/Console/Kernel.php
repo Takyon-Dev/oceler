8312f2d29ca8b9a892bf39aaad471eabf071fe36
Gabe Mansur <gabe_mansur@harvard.edu>
2018-10-24 09:30:26 -0400

    Changed scheduling of qual process from 30 to 5 minutes

--- app/Console/Kernel.php
+++ app/Console/Kernel.php
@@ -41,7 +41,7 @@ class Kernel extends ConsoleKernel
                  ->everyThirtyMinutes();
 
         $schedule->command('MTurkProcessQualification')
-                 ->everyThirtyMinutes();
+                 ->everyFiveMinutes();
 
 
     }
