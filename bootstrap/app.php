898408f8c4e66f5a160a9676dd39ebcc6d8b4171
carlosjimenezdev <carlosjimenezdev@gmail.com>
2025-05-10 23:06:22 -0600

    u

--- bootstrap/app.php
+++ bootstrap/app.php
@@ -28,17 +28,17 @@ $app = new Illuminate\Foundation\Application(
 
 $app->singleton(
     Illuminate\Contracts\Http\Kernel::class,
-    oceler\Http\Kernel::class
+    App\Http\Kernel::class
 );
 
 $app->singleton(
     Illuminate\Contracts\Console\Kernel::class,
-    oceler\Console\Kernel::class
+    App\Console\Kernel::class
 );
 
 $app->singleton(
     Illuminate\Contracts\Debug\ExceptionHandler::class,
-    oceler\Exceptions\Handler::class
+    App\Exceptions\Handler::class
 );
 
 /*
