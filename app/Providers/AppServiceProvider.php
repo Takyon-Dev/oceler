5398118c6de677aa19141ed7b4a6b5a4d73343c7
carlosjimenezdev <carlosjimenezdev@gmail.com>
2025-05-10 23:10:22 -0600

    u

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -7,9 +7,8 @@ use Illuminate\Support\Facades\Schema;
 use Illuminate\Support\Facades\Blade;
 use Illuminate\Support\Facades\View;
 use Illuminate\Support\Facades\URL;
-use Illuminate\Foundation\Support\Providers\ServiceProvider as BaseServiceProvider;
 
-class AppServiceProvider extends BaseServiceProvider
+class AppServiceProvider extends ServiceProvider
 {
     /**
      * Register any application services.
